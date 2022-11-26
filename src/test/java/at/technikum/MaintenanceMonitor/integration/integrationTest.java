b0eef4b763e7bd07f6e0fa98b71ba9ce65f31d03
Roman REITER <reiter@live.at>
2022-11-26 19:53:06 +0100

    #12 implement integration-test for the post method
    a body with a message is transmitted using the Post method. After the response body arrives, the JSON contents of the two bodies are compared. The message must be the same and the difference between sending and receiving time must be less than one second.

---
+++ src/test/java/at/technikum/MaintenanceMonitor/integration/integrationTest.java
@@ -0,0 +1,53 @@
+package at.technikum.MaintenanceMonitor.integration;
+
+import at.technikum.MaintenanceMonitor.dto.Message;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class integrationTest {
+
+    @Test
+    public void testPost() throws IOException, InterruptedException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+        String requestBody = objectMapper.writeValueAsString("Test");
+        Message messagee = new Message("Test",LocalDateTime.now());
+
+        HttpClient client = HttpClient.newBuilder().build();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("http://localhost:8080/uptime/setMessage"))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofString(requestBody))
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        JsonNode expectedJsonNode = objectMapper.readTree(objectMapper.writeValueAsString(messagee));
+        String expectedMessage = expectedJsonNode.get("message").textValue();
+        String expectedLastUpdateTime = expectedJsonNode.get("lastUpdateTime").textValue();
+        JsonNode actualJsonNode = objectMapper.readTree(response.body());
+        String actualMessage = actualJsonNode.get("message").textValue();
+        String actualLastUpdateTime = actualJsonNode.get("lastUpdateTime").textValue();
+
+        LocalDateTime actualdateTime = LocalDateTime.parse(actualLastUpdateTime);
+        LocalDateTime expecteddateTime = LocalDateTime.parse(expectedLastUpdateTime);
+        Duration duration = Duration.between(expecteddateTime, actualdateTime);
+
+        assertTrue(expectedMessage.equals(actualMessage) && duration.getSeconds()==0);
+    }
+}
